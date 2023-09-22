d30a6d590b0369ed24d95f5efcd0c8198ef6e1c9
Manuel <manu@meetsam.io>
2023-09-22 08:31:56 +0200

    Add instructions

---
+++ src/data/cards.js
@@ -0,0 +1,111 @@
+export default [
+  { "slug": "c",
+    "title": "Curiosity",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
+    <path d="M21 21l-6 -6"></path>
+    </svg>`),
+    "title_css": "from-orange-400 to-amber-600",
+    "icon_css": "text-amber-600",
+    "description": "I have plenty of things to investigate and to think about"},
+  { "slug": "h",
+    "title": "Honor",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-medal-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M9 3h6l3 7l-6 2l-6 -2z"></path>
+    <path d="M12 12l-3 -9"></path>
+    <path d="M15 11l-3 -8"></path>
+    <path d="M12 19.5l-3 1.5l.5 -3.5l-2 -2l3 -.5l1.5 -3l1.5 3l3 .5l-2 2l.5 3.5z"></path>
+    </svg>`),
+    "title_css": "from-cyan-400 to-sky-600",
+    "icon_css": "text-sky-600",
+    "description": "I feel proud that my personal values are reflected in how I work."},
+  { "slug": "a",
+    "title": "Acceptance",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-thumb-up" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"></path>
+    </svg>`),
+    "title_css": "from-yellow-400 to-lime-600",
+    "icon_css": "text-lime-600",
+    "description": "The people around me approve of what I do and who I am."},
+  { "slug": "m",
+    "title": "Mastery",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chess-king" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M8 16l-1.447 .724a1 1 0 0 0 -.553 .894v2.382h12v-2.382a1 1 0 0 0 -.553 -.894l-1.447 -.724h-8z"></path>
+    <path d="M8.5 16a3.5 3.5 0 1 1 3.163 -5h.674a3.5 3.5 0 1 1 3.163 5z"></path>
+    <path d="M9 6h6"></path>
+    <path d="M12 3v8"></path>
+    </svg>`),
+    "title_css": "from-teal-400 to-cyan-600",
+    "icon_css": "text-cyan-600",
+    "description": "My work challenges my competence but it is still within my abilities."},
+  { "slug": "p",
+    "title": "Power",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-battery-charging" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M16 7h1a2 2 0 0 1 2 2v.5a.5 .5 0 0 0 .5 .5a.5 .5 0 0 1 .5 .5v3a.5 .5 0 0 1 -.5 .5a.5 .5 0 0 0 -.5 .5v.5a2 2 0 0 1 -2 2h-2"></path>
+    <path d="M8 7h-2a2 2 0 0 0 -2 2v6a2 2 0 0 0 2 2h1"></path>
+    <path d="M12 8l-2 4h3l-2 4"></path>
+    </svg>`),
+    "title_css": "from-amber-400 to-yellow-600",
+    "icon_css": "text-yellow-600",
+    "description": "There’s enough room for me to influence what happens around me."},
+  { "slug": "f",
+    "title": "Freedom",
+    "svg":(slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-air-balloon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M10 19m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v1a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
+    <path d="M12 16c3.314 0 6 -4.686 6 -8a6 6 0 1 0 -12 0c0 3.314 2.686 8 6 8z"></path>
+    <path d="M12 9m-2 0a2 7 0 1 0 4 0a2 7 0 1 0 -4 0"></path>
+    </svg>`),
+    "title_css": "from-red-400 to-orange-600",
+    "icon_css": "text-orange-600",
+    "description": "I am independent of others with my work and my responsibilities."},
+  { "slug": "r",
+    "title": "Relatedness",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-messages" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10"></path>
+    <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2"></path>
+    </svg>`),
+    "title_css": "from-emerald-400 to-teal-600",
+    "icon_css": "text-teal-600",
+    "description": "I have good social contacts with the people in my work."},
+  { "slug": "o",
+    "title": "Order",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-category-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M14 4h6v6h-6z"></path>
+    <path d="M4 14h6v6h-6z"></path>
+    <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+    <path d="M7 7m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+    </svg>`),
+    "title_css": "from-pink-400 to-rose-600",
+    "icon_css": "text-rose-600",
+    "description": "There are enough rules and policies for a stable environment."},
+  { "slug": "g",
+    "title": "Goal",
+    "svg": (slot) => (`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-flag-3" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+    <path d="M5 14h14l-4.5 -4.5l4.5 -4.5h-14v16"></path>
+    </svg>`),
+    "title_css": "from-indigo-400 to-violet-600",
+    "icon_css": "text-violet-600",
+    "description": "My purpose in life is reflected in the work that I do."},
+  { "slug": "s",
+    "title": "Status",
+    "svg": ()=>(`<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-vip" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+      <path d="M3 5h18"></path>
+      <path d="M3 19h18"></path>
+      <path d="M4 9l2 6h1l2 -6"></path>
+      <path d="M12 9v6"></path>
+      <path d="M16 15v-6h2a2 2 0 1 1 0 4h-2"></path>
+    </svg>`),
+    "title_css": "from-fuchsia-400 to-pink-600",
+    "icon_css": "text-pink-600",
+    "description": "My position is good, and recognized by the people who work with me."}
+];
